361c5001ffa9b80aa7b98672e4d73ee4af813f8d
Guelmis Cortina <guelmiscortina@Guelmis-Cortinas-MacBook-Pro.local>
2015-10-12 20:27:03 -0400

    Added The stock price code to server.rb

--- server.rb
+++ server.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
+require 'HTTParty'
+require 'nokogiri'
 
 get '/' do
-	File.open("./public/hello.txt").read
+response = HTTParty.get("http://finance.yahoo.com/q?s=googl")
+ body = response.body
+dom = Nokogiri::HTML(body)
+stock_price = dom.xpath("//span[@id='yfs_l84_googl']").first.content
+"The current stock price of Google $#{stock_price}"
 end
 
