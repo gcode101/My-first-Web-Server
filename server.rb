365507c9531adff537e1925f42d71f4ac8e535b1
Guelmis Cortina <guelmiscortina@Guelmis-Cortinas-MacBook-Pro.local>
2015-10-12 21:08:22 -0400

    Added stock choosing abilities from the url

--- server.rb
+++ server.rb
@@ -3,10 +3,12 @@ require 'HTTParty'
 require 'nokogiri'
 
 get '/' do
-response = HTTParty.get("http://finance.yahoo.com/q?s=googl")
- body = response.body
-dom = Nokogiri::HTML(body)
-stock_price = dom.xpath("//span[@id='yfs_l84_googl']").first.content
-"The current stock price of Google $#{stock_price}"
+	stock = params[:stock]
+	symbol = stock.downcase
+	response = HTTParty.get("http://finance.yahoo.com/q?s=#{symbol}")
+	body = response.body
+	dom = Nokogiri::HTML(body)
+	stock_price = dom.xpath("//span[@id='yfs_l84_#{symbol}']").first.content
+	"The current stock price is $#{stock_price}"
 end
 
